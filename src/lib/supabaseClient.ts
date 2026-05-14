970857c85ed9d294f0eb32ecc15b74218175d1b4
Muhammad Zamran <zamran.qazi@gmail.com>
2026-05-14 23:42:10 +0500

    Change hasValidSupabaseKeys to a function

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -4,4 +4,4 @@ const supabaseUrl = "https://ohvznvhbplennofykffw.supabase.co"
 const supabaseAnonKey = "EyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9odnpudmhicGxlbm5vZnlrZmZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3NjkwNTYsImV4cCI6MjA5NDM0NTA1Nn0.aBVMELsgEAZR6afOafwyuKshlIvCSxVF_G1nrQOA4J8"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
-export const hasValidSupabaseKeys = true;
+export const hasValidSupabaseKeys = () => true;
